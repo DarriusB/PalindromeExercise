6000cedeec5bfae1908710c157695572dc61f308
DarriusB <darrius.bradford@gmail.com>
2022-05-16 11:08:45 -0700

    Exercise Complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string text)
+        {
+            var result = text.ToLower();
+            return result.SequenceEqual(result.Reverse());
+
+            //throw new NotImplementedException();
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,32 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("anna", true)]
+        [InlineData("mom", true)]
+        [InlineData("RADAR", true)]
+        [InlineData("madam", true)]
+        [InlineData("Anna", true)]
+        [InlineData("Refer", true)]
+        [InlineData("racecar", true)]
+        [InlineData("donald", false)]
+        [InlineData("tomorrow", false)]
 
+        public void IsAPalindromeTest(string text, bool expected)
+        {
+            // Arrange
+            var wordSmith = new WordSmith();
+
+            // Act
+            var actual = wordSmith.IsAPalindrome(text);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
